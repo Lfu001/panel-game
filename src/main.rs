72fe065e26a603a35663c23e9db5ad6d1559d6d5
Lfu001 <goodspeed913@gmail.com>
2025-03-16 19:28:50 +0900

    feat: serve static files from frontend/dist

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@ mod estimator;
 mod types;
 
 use crate::api::{estimate::estimate, index::index};
+use actix_files::Files;
 use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(index).service(estimate);
+        cfg.service(index)
+            .service(estimate)
+            .service(Files::new("/", "frontend/dist"));
     };
 
     Ok(config.into())
